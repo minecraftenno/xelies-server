a0dbb4c568fd9e4cd44dcf0292b81ab3d97e056a
NotPunchnox <74469788+NotPunchnox@users.noreply.github.com>
2021-06-04 21:09:33 +0200

    Update jwt.js

--- src/middlewares/jwt.js
+++ src/middlewares/jwt.js
@@ -3,7 +3,7 @@ jwt = require('jsonwebtoken')
 
 module.exports = (req, res, next)=> {
     if(!req.headers.authorization) {
-        return res.status(400).send(ApiError.badrequest())
+        return res.status(400).send(ApiError.badrequest)
     }
     try {
         req.user_id = jwt.verify(req.headers.authorization, process.env.SECRET || require('../../c.json').SECRET)
