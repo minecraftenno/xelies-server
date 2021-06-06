cf038729aee77ca3e8648887c259e9db8d622a07
NotPunchnox <74469788+NotPunchnox@users.noreply.github.com>
2021-06-06 12:18:24 +0200

    Login => fonctionnel, opti

--- src/utils/auth/login.js
+++ src/utils/auth/login.js
@@ -1,68 +1,50 @@
 const jwt = require('jsonwebtoken'),
-    ApiResponse = require('../../helpers/ApiResponse'),
-    user = require('../../models/user.model'),
-    crypt = require('../../crypto/crypto'),
-    bcrypt = require('bcrypt');
+ApiResponse = require('../../helpers/ApiResponse'),
+user = require('../../models/user.model'),
+crypt = require('../../crypto/crypto'),
+bcrypt = require('bcrypt');
 
 module.exports = (app) => {
-    app.post('/login', (req, res) => {
-        const {
-            username,
-            email,
-            password
-        } = req.body
+  app.post('/login', (req, res) => {
+    const {
+      email,
+      password
+    } = req.body
 
-        if(typeof username == "undefined" && typeof email == "undefined") {
-            return res.status(503).json(ApiResponse.badrequest)
-        }
+    if (!email) return res.status(503).json(ApiResponse.badrequest)
+    if (!email.includes('@')) return res.status(503).json(ApiResponse.badrequest)
+    user.find({}, (err, doc) => {
+      if (err) return res.status(503).json(ApiResponse.error)
 
-        if(typeof username != "undefined") {
-            user.findOne({username: username}, (err, doc) => {
-                if(err) return res.status(503).json(ApiResponse.error)
-                if(doc) {
-                    bcrypt.compare(password, doc.password, (err, result) => {
-                        if(result == true) {
-                            let token = jwt.sign({
-                                ID: doc._id
-                            }, doc.password, {
-                                expiresIn: '24h'
-                            })
-                            res.status(203).json({
-                                token: token
-                            })
-                        } else {
-                            return res.status(403).json(ApiResponse(403, "Invalid username or password."))
-                        }
-                    });
-                } else {
-                    return res.status(403).json(ApiResponse(403, "Invalid username or password."))
-                }
-            });
-        } else if(typeof email != "undefined") {
-            user.findOne({email: email}, (err, doc) => {
-                if(err) return res.status(503).json(ApiResponse.error)
-                if(doc) {
-                    bcrypt.compare(password, doc.password, (err, result) => {
-                        if(result == true) {
-                           
-                            let token = jwt.sign({
-                                ID: doc._id
-                            }, doc.password, {
-                                expiresIn: '24h'
-                            });
-                            res.status(203).json({
-                                token: token
-                            });
-                        } else {
-                            return res.status(403).json(ApiResponse(403, "Invalid email or password."))
-                        }
-                    });
-                } else {
-                    return res.status(403).json(ApiResponse(403, "Invalid email or password."))
-                }
-            });
-        } else {
-            return res.status(400).json(ApiResponse.badrequest)
+      const checkemail = doc.find(a => {
+        if (crypt.decrypt({
+          iv: a.email.iv,
+          content: a.email.content
+        }, key) === email) return {
+          iv: a.email.iv,
+          content: a.email.content
         }
+      })
+      user.findOne({
+        'eimal.iv': {
+          $in: checkemail.iv
+        }
+      }).exec((e, d) => {
+        if (e) return res.status(503).json(ApiResponse.error)
+        if(!d) return res.status(403).json(ApiResponse(403, "Invalid email"))
+        bcrypt.compare(password, d.password, (err, result) => {
+          if (result) {
+            let token = jwt.sign({
+              ID: doc._id
+            }, doc.password, {
+              expiresIn: '24h'
+            });
+            res.status(203).json({
+              token: token
+            });
+          } else return res.status(403).json(ApiResponse(403, "Invalid password."))
+        })
+      })
     })
+  })
 }
