fce452a4d6b7dad55b485b3f17430715ed1c5c38
RamokTVL <steamxaiden@gmail.com>
2021-06-09 20:31:25 +0200

    Fix login

--- src/utils/auth/login.js
+++ src/utils/auth/login.js
@@ -1,61 +1,55 @@
 const jwt = require('jsonwebtoken'),
 ApiError = require('../../helpers/ApiError'),
-user = require('../../models/guild.model'),
+user = require('../../models/user.model'),
 crypt = require('../../crypto/crypto'),
 bcrypt = require('bcrypt'),
-key = process.env.SECRET || require('../../../c.json').SECRET;
+key = process.env.SECRET || require('../../../c.json').SECRET
 
-module.exports = (app) => {
-  app.post('/login', (req, res) => {
-    const {
-      email,
-      password
-    } = req.body;
+module.exports = app => {
+  app.get('/login', (req, res) => {
+    if(!JSON.stringify(req.body)) return res.status(405).json(ApiError.badrequest)
+    const { email, password } = req.body
 
-    if (!email || !password) return res.status(400).json(ApiError.badrequest);
-    if (!email.includes('@')) return res.status(400).json(ApiError.badrequest);
+    if (!email) return res.status(404).json(new ApiError(404, 'Missing email!'))
+    if (!password) return res.status(404).json(new ApiError(404, 'Missing password!'))
+
+    if (!email.includes('@')) return res.status(400).json(ApiError.badrequest)
+
+
     user.find({}, (err, doc) => {
       if (err) return res.status(503).json(ApiError.error)
 
       const checkemail = doc.find(a => {
-        if (crypt.decrypt({
-          iv: a.email.iv,
-          content: a.email.content
-        }, key) === email) return {
+        if (crypt.decrypt({iv: a.email.iv,content: a.email.content}, key) === email) return {
             status: true,
             iv: a.email.iv,
             content: a.email.content
           }
-      });
-      if(!checkemail) {
-        return res.status(403).json(new ApiError(403, "Invalid email"));
-      } else {
-        console.log(checkmail)
-      }
+      })
+      if(!checkemail) return res.status(404).json(new ApiError(404, "Invalid email"))
 
       user.findOne({
-        "email.iv": checkemail.iv
+        "email.iv": checkemail.email.iv
       }).exec((e, d) => {
-        if (e) return res.status(503).json(ApiError.error), console.error(e)
-        console.log("doc: ", d)
-        if(!d) return res.status(403).json(new ApiError(403, "Invalid email"));
+        if (e) return res.status(503).json(ApiError.error)
+        if(!d) return res.status(500).json(ApiError.error)
         
-        bcrypt.compare(password, d.password, (err, result) => {
-          if (result == true) {
-
+        bcrypt.compare(password, d.password, (e, r) => {
+          if(e) return res.status(500).json(ApiError.error)
+          if (r) {
             let token = jwt.sign({
-              ID: doc._id
-            }, doc.password, {
+              ID: d._id
+            }, d.password, {
               expiresIn: '24h'
-            });
+            })
 
             res.status(203).json({
               token: token
-            });
+            })
 
-          } else return res.status(403).json(new ApiError(403, "Invalid password."))
-        });
-      });
-    });
+          } else return res.status(403).json(new ApiError(403, "Invalid password"))
+        })
+      })
+    })
   })
 }
