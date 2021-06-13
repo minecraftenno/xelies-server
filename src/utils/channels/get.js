f5a2b8fabb09e06d023c50fc47223e7270b622f6
RamokTVL <steamxaiden@gmail.com>
2021-06-13 02:53:10 +0200

    Bordel le minify => beautify a niqué toutes mes variables 🤪🤪🤪

--- src/utils/channels/get.js
+++ src/utils/channels/get.js
@@ -1,47 +1,81 @@
-const ApiError = require("../../helpers/ApiError"),
-    auth = require("../../middlewares/authorization"),
-    CheckAuth = require("../../middlewares/jwt"),
-    guilds = require("../../models/guild.model"),
+const ApiError = require('../../helpers/ApiError'),
+    auth = require('../../middlewares/authorization'),
+    CheckAuth = require('../../middlewares/jwt'),
+    guilds = require('../../models/guild.model'),
     user = require("../../models/user.model"),
     channels = require("../../models/channel.model"),
-    bcrypt = require("bcrypt");
-module.exports = (r => {
-    r.get("/channels/:channelid", auth, async (r, s) => {
-        if (!r.params) return s.status(400).json(ApiError.badrequest);
-        if (!r.password) return s.status(401).json(ApiError.unauthorized);
+    bcrypt = require("bcrypt")
+
+module.exports = (app) => {
+    app.get('/channels/:channelid', auth, async (req, res) => {
+
+        if (!req.params) return res.status(400).json(ApiError.badrequest)
+        if (!req.password) return res.status(401).json(ApiError.unauthorized)
         const {
-            channelid: e
-        } = r.params, {
-            nsfw: i,
-            password: t
-        } = r.body, o = r.headers.authorization || r.signedCookies.Authorization;
-        let n = require("../../../middlewares/jwt")(o, r.password);
-        if (!n.ID) return s.status(401).json(ApiError.unauthorized);
-        user.findById(n.ID, (r, o) => r ? s.send(ApiError.error).status(500) : o ? void channels.findById(e, (r, e) => r ? s.send(ApiError.error).status(500) : e ? !e.nsfw || i && 1 == i ? void(null != e.password ? (null == t && s.send(ApiError.unauthorized).status(401), bcrypt.compare(t, e.password, (r, i) => {
-            if (r) return s.send(ApiError.error).status(500);
-            i ? s.json({
-                _id: e._id,
-                name: e.name,
-                description: e.description,
-                author: e.author,
-                type: e.type,
-                permissions: e.permissions,
-                position: e.position,
-                CreatedAt: e.CreatedAt,
-                messages: e.messages
-            }) : s.send(ApiError.unauthorized).status(401)
-        })) : s.json({
-            _id: e._id,
-            name: e.name,
-            description: e.description,
-            author: e.author,
-            type: e.type,
-            permissions: e.permissions,
-            position: e.position,
-            CreatedAt: e.CreatedAt,
-            messages: e.messages
-        })) : s.send(ApiError.forbidden).send(403) : s.send(ApiError.badrequest).status(400)) : s.send(ApiError.unauthorized).status(401));
-        let d = await guilds.findById(code);
-        return d ? await d.members.find(r => r.user.id === n.ID) ? s.status(200).json(d) : s.status(403).json(ApiError.forbidden) : s.status(404).json(ApiError.notfound)
+            channelid
+        } = req.params,
+            {
+                nsfw,
+                password
+            } = req.body,
+            authorization = req.headers.authorization || req.signedCookies.Authorization
+        let decoded = require('../../../middlewares/jwt')(authorization, req.password)
+        if (!decoded.ID) return res.status(401).json(ApiError.unauthorized)
+
+        user.findById(decoded.ID, (err, user) => {
+            if (err) return res.send(ApiError.error).status(500);
+            if (!user) return res.send(ApiError.unauthorized).status(401);
+
+            channels.findById(channelid, (err, channel) => {
+                if (err) return res.send(ApiError.error).status(500);
+                if (!channel) return res.send(ApiError.badrequest).status(400);
+                if (channel.nsfw) {
+                    if (!nsfw || nsfw != true) return res.send(ApiError.forbidden).send(403);
+                }
+                if (channel.password != null) {
+                    if (password == null) {
+                        res.send(ApiError.unauthorized).status(401)
+                    }
+                    bcrypt.compare(password, channel.password, (err, result) => {
+                        if (err) return res.send(ApiError.error).status(500);
+                        if (result) {
+                            res.json({
+                                _id: channel._id,
+                                name: channel.name,
+                                description: channel.description,
+                                author: channel.author,
+                                type: channel.type,
+                                permissions: channel.permissions,
+                                position: channel.position,
+                                CreatedAt: channel.CreatedAt,
+                                messages: channel.messages
+                            })
+                        } else {
+                            res.send(ApiError.unauthorized).status(401);
+                        }
+                    });
+                } else {
+                    res.json({
+                        _id: channel._id,
+                        name: channel.name,
+                        description: channel.description,
+                        author: channel.author,
+                        type: channel.type,
+                        permissions: channel.permissions,
+                        position: channel.position,
+                        CreatedAt: channel.CreatedAt,
+                        messages: channel.messages
+                    })
+                }
+            });
+        });
+
+        let server = await guilds.findById(code)
+        if (!server) return res.status(404).json(ApiError.notfound)
+        if (!await server.members.find(a => a.user.id === decoded.ID)) return res.status(403).json(ApiError.forbidden)
+
+
+        return res.status(200).json(server)
+
     })
-});
+}
